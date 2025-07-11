d8be54637e580d2e8a8313b1ad147ef6a5a86642
Hugo Perez <hugoperez510@hotmail.com>
2025-07-11 02:03:08 -0600

    Actualizar la referencia de desugar_jdk_libs en libs.versions.toml y corregir la implementación en build.gradle.kts

--- app-tv/build.gradle.kts
+++ app-tv/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    coreLibraryDesugaring(libs.desugar.jdk.libs)
+    coreLibraryDesugaring(libs.android.desugar.jdk.libs)
 
     // Compose TV
     implementation(libs.androidx.tv.foundation)
