35e0458466cb7bddd2287e0276265cfb0d7bb913
Hugo Perez <hugoperez510@hotmail.com>
2025-07-13 23:05:19 -0600

    feat: Agregar declaración de repositorios en la configuración del módulo shared-domain

--- shared-domain/build.gradle.kts
+++ shared-domain/build.gradle.kts
@@ -3,6 +3,11 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
 }
 
+repositories {
+    mavenCentral()
+    google()
+}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
